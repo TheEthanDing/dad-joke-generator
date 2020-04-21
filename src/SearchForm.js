33031114c56d36817b201c4d32abe2b5e57f1907
Ethan Ding <43360483+ethan-ding@users.noreply.github.com>
2020-04-21 18:43:35 -0400

    CSS resources unavailable

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./SearchForm.css";
 const SearchForm = props => {
   const onSubmit = event => {
     event.preventDefault();
@@ -7,16 +7,22 @@ const SearchForm = props => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} className="search-form">
       <input
         type="text"
         placeholder="Enter search term..."
         onChange={event => props.onSearchValueChange(event.target.value)}
       />
-      <button disabled={props.isSearching}>Search</button>
-      <button onClick={props.onSingleSearchClick} disabled={props.isSearching}>
-        I'm Feeling Funny
-      </button>
+      <div>
+        <button disabled={props.isSearching}>Search</button>
+
+        <button
+          onClick={props.onSingleSearchClick}
+          disabled={props.isSearching}
+        >
+          I'm Feeling Funny
+        </button>
+      </div>
     </form>
   );
 };
--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="App">
         <SearchForm
           onFormSubmit={this.searchJokes}
           onSearchValueChange={this.onSearchChange}

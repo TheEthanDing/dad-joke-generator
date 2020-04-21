80880428f8209ebb4ce1915c6afe1229764cf0db
Ethan Ding <43360483+ethan-ding@users.noreply.github.com>
2020-04-21 10:37:29 -0400

    saving and sending search term to API call

--- src/index.js
+++ src/index.js
@@ -10,20 +10,20 @@ class App extends React.Component {
     super();
 
     this.state = {
-      joke: null,
+      searchTerm: "",
+      jokes: [],
       isFetchingJoke: false
     };
 
     this.onTellJoke = this.onTellJoke.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
   }
 
-  componentDidMount() {
-    this.fetchJoke();
-  }
-
-  fetchJoke() {
+  searchJokes() {
     this.setState({ isFetchingJoke: true });
-    fetch("https://icanhazdadjoke.com/", {
+
+    fetch(`https://icanhazdadjoke.com/search?term=${this.state.searchTerm}`, {
       method: "GET",
       headers: {
         Accept: "application/json"
@@ -31,24 +31,47 @@ class App extends React.Component {
     })
       .then(response => response.json())
       .then(json => {
+        const jokes = json.results;
+        console.log("jokes", jokes);
         this.setState({
-          joke: json.joke,
+          jokes,
           isFetchingJoke: false
         });
       });
   }
 
   onTellJoke() {
-    this.fetchJoke();
+    this.searchJokes();
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onSearchSubmit(event) {
+    event.preventDefault();
+    this.searchJokes();
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.onTellJoke} disabled={this.state.isFetchingJoke}>
-          Tell me a joke
-        </button>
-        <p>{this.state.isFetchingJoke ? "Loading joke..." : this.state.joke}</p>
+        <form onSubmit={this.onSearchSubmit}>
+          <input
+            type="text"
+            placeholder="Enter search term..."
+            onChange={this.onSearchChange}
+          />
+          <button>Search</button>
+          <button
+            onClick={this.onTellJoke}
+            disabled={this.state.isFetchingJoke}
+          >
+            Tell me a joke
+          </button>
+        </form>
+        <p>{this.state.jokes.toString()}</p>
+        <p>search term: {this.state.searchTerm}</p>
       </div>
     );
   }
